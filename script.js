6479a9a4e2b2cc7069948497a94ae29acad53dbe
trialnerr <54381853+trialnerr@users.noreply.github.com>
2023-12-08 20:30:22 -0500

    Added color input, rainbow button toggle

--- script.js
+++ script.js
@@ -1,53 +1,104 @@
-const grid = document.querySelector(".grid");
-const value = document.querySelector('#value'); 
-const input = document.querySelector('#input'); 
+let isMouseDown = false;
 
-//create a div of width 100/n %
+const grid = document.querySelector(".grid");
+const value = document.querySelector("#value");
+const input = document.querySelector("#input");
+const colorPicker = document.querySelector("#color-picker");
+const rainbowBtn = document.querySelector(".rainbow");
+console.log(colorPicker.value);
+
+grid.addEventListener("mousedown", () => {
+  isMouseDown = true;
+});
+
 
 function createGrid(n) {
   for (let i = 0; i < n; i++) {
-    const row = document.createElement('div');
-    row.setAttribute('class', 'row')
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
     for (let j = 0; j < n; j++) {
       const square = document.createElement("div");
       square.setAttribute("class", "square");
       square.style.width = `calc(800px/${n})`;
       square.style.height = `calc(800px/${n})`;
 
-      
       row.appendChild(square);
     }
     grid.appendChild(row);
   }
- 
+}
+
+function removeGrid() {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+}
+
+function changeGridSize() {
+  value.textContent = input.value;
+  input.addEventListener("input", (event) => {
+    value.textContent = event.target.value;
+    removeGrid();
+    createGrid(event.target.value);
+    applyColorListener();
+  });
+}
+
+function applyColorListener() {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseenter", handleMouseEnter);
+    square.addEventListener("click", handleClick);
+  });
+}
+
+function handleMouseEnter(e) {
+  if (isMouseDown) {
+    if (rainbowBtn.classList.contains("isRainbowMode")) {
+      const randomColor = getRandomColor()
+      e.target.style.background = randomColor;
+    } else {
+      e.target.style.background = colorPicker.value;
+    }
+  }
+}
+
+function handleClick(e){
+
+  if (rainbowBtn.classList.contains("isRainbowMode")) {
+    const randomColor = getRandomColor()
+    e.target.style.background = randomColor;
+  } else {
+    e.target.style.background = colorPicker.value;
+  }
+}
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function setRainbowMode() {
+  rainbowBtn.addEventListener("click", () => {
+    rainbowBtn.classList.toggle("isRainbowMode");
+  });
 }
 
 createGrid(16);
-
-function clearGrid(){
-    while(grid.firstChild){
-        grid.removeChild(grid.firstChild);
-    }
-}
- 
-
-function changeGridSize() {
-    value.textContent = input.value;
-    input.addEventListener("input", (event) => {
-      value.textContent = event.target.value;
-      clearGrid();
-      createGrid(event.target.value);
-    });
-}  
-
 changeGridSize();
+applyColorListener();
+colorPicker.addEventListener("change", applyColorListener);
+setRainbowMode();
+//create an option to paint in color mode
 
+//add an event listener to all the square divs
+//on key down they should apply selected color as background
+// on key down again toggle the color
 
-
-//create an option to paint in color mode 
-    //add an event listener to all the square divs
-    //on key down they should apply selected color as background 
-    //
-//create an option to paint in rainbow mode 
+//create an option to paint in rainbow mode
 //option to clear the board
 //option to clear a cell
